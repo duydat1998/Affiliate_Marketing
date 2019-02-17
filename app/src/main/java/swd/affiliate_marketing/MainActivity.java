e9f51c7955c28fc892fc19da2eb7f101d44eb86f
Nguyễn Duy Đạt <32428939+duydat1998@users.noreply.github.com>
2019-02-17 13:55:16 +0700

    Initial commit

---
+++ app/src/main/java/swd/affiliate_marketing/MainActivity.java
@@ -0,0 +1,13 @@
+package swd.affiliate_marketing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
