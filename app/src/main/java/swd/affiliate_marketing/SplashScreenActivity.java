1555c7a1bd83eee0f9d61b831f763c7653b0c8a0
nguyendptse62666 <nguyendptse62666@fpt.edu.vn>
2019-02-27 22:07:20 +0700

    update
    Add splash sreen and splash screen time delay fix.

--- app/src/main/java/swd/affiliate_marketing/SplashScreenActivity.java
+++ app/src/main/java/swd/affiliate_marketing/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package swd.affiliate_marketing;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -9,21 +10,17 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Thread(new Runnable() {
-            @Override
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
             public void run() {
-                try{
-                    Thread.sleep(2000);
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
-        }).start();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        }, 3000);
+
     }
 }
