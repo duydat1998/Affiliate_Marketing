1a4c749614ef5f49d0373b68fdb8c7217e8ef073
Nguyễn Duy Đạt <32428939+duydat1998@users.noreply.github.com>
2019-03-01 10:18:30 +0700

    add basci layout

--- app/src/main/java/swd/affiliate_marketing/LoginActivity.java
+++ app/src/main/java/swd/affiliate_marketing/LoginActivity.java
@@ -1,7 +1,9 @@
 package swd.affiliate_marketing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,7 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    }
+
+    public void clickToLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/swd/affiliate_marketing/MainActivity.java
+++ app/src/main/java/swd/affiliate_marketing/MainActivity.java
@@ -1,13 +1,58 @@
 package swd.affiliate_marketing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_campaign:
+                    loadFragment(new CampaignFragment());
+                    return true;
+                case R.id.navigation_report:
+                    loadFragment(new ReportFragment());
+                    return true;
+                case R.id.navigation_profile:
+                    loadFragment(new ProfileFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new CampaignFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
 }
